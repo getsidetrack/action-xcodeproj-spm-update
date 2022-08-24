1c9db0009b63d58fa4a538b501957eed39cd1f7e
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2022-08-24 23:22:20 +0100

    Patches from Testing
    After doing a number of testing a couple bugs were identified with recent tweaks. This appears to now be operational with both projects and workspaces.

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,23 +41,24 @@ fi
 
 # Identify `Package.resolved` location
 if [ ! -z "$workspaceName" ]; then
-  RESOLVED_PATH=$(find . -type f -name "Package.resolved" | grep -v "*/*.xcworkspace/*")
+  RESOLVED_PATH=$(find $workspaceName -type f -name "Package.resolved" | grep -v "*/*.xcworkspace/*")
 else
   RESOLVED_PATH=$(find . -type f -name "Package.resolved" | grep -v "*/*.xcodeproj/*")
 fi
 
 CHECKSUM=$(shasum "$RESOLVED_PATH")
 echo "Identified Package.resolved at '$RESOLVED_PATH'."
+echo "Checksum: $CHECKSUM."
 
 # Define Xcodebuild Inputs
 if [ ! -z "$workspaceName" ]; then
-  xcodebuildInputs = "-workspace \"$workspaceName\" -scheme \"$scheme\""
+  xcodebuildInputs="-workspace $workspaceName -scheme $scheme"
 else
-  xcodebuildInputs = ""
+  xcodebuildInputs=""
 fi
 
 # Cleanup Caches
-DERIVED_DATA=$(xcodebuild ${xcodebuildInputs} -showBuildSettings -disableAutomaticPackageResolution -disablePackageRepositoryCache | grep -m 1 BUILD_DIR | grep -oE "\/.*" | sed 's|/Build/Products||')
+DERIVED_DATA=$(xcodebuild ${xcodebuildInputs} -showBuildSettings -disableAutomaticPackageResolution -skipPackageUpdates | grep -m 1 BUILD_DIR | grep -oE "\/.*" | sed 's|/Build/Products||')
 rm -rf "$DERIVED_DATA"
 
 # If `forceResolution`, then delete the `Package.resolved`
